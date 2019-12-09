6b372777a427e97356bad27052e3013898f56d34
Derek Harmon <loresayer@msn.com>
2019-12-08 21:49:05 -0800

    Prototype YesNoLayout and state transitions up to IDK.

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,98 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+
+final String rsTitle = 'Flutter Expert System';
+final TextStyle whT48 = TextStyle(color: Colors.white, fontSize: 48);
+final TextStyle bkT48 = TextStyle(color: Colors.black, fontSize: 48);
 
 void main() => runApp(App());
 
-class App extends StatelessWidget {
+class YesNoLayout extends StatefulWidget {
+  YesNoLayout({Key key, this.tree, this.ki}) : super(key: key);
+  final Map<int,String> tree;
+  final int ki;
+
+  @override
+  _YesNoState createState() => _YesNoState();
+
+  int next(bool b) {
+    print('next(' + b.toString() + '): k_(i+1) is ' + (2*ki + (b ? 1 : 0)).toString() + ' = 2*' + ki.toString() + '+' + (b ? 1 : 0).toString() + ')\n');
+    return 2*ki + (b ? 1 : 0);
+  }
+
+  String vi(int ki) {
+    return tree.containsKey(ki) ? tree[ki] : null;
+  }
+}
+
+class _YesNoState extends State<YesNoLayout> {
+  Widget makeMaterialButton(bool b, double w, double h) {
+    return MaterialButton(
+              height: h,
+              minWidth: w,
+              child: Text(b ? 'Yes' : 'No', style: whT48),
+              color: b ? Colors.green : Colors.deepOrange,
+              onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => YesNoLayout(
+                          tree: widget.tree,
+                          ki: widget.next(b))
+                  )
+              ),
+            );
+  }
+
+  Text getBodyText(Orientation o) {
+    var lead = (o == Orientation.portrait) ? '\n\n\n' : '\n';
+    var v = widget.vi(widget.ki);
+    return Text((v == null) ? 'IDK' : lead + v, style: bkT48);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Expert System',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    final sz = MediaQuery.of(context).size;
+    final wBtn = sz.width/2.2;
+    final hBtn = sz.height/10.2;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(rsTitle),
       ),
-      home: Home(title: 'Query'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: getBodyText(MediaQuery.of(context).orientation),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              makeMaterialButton(false, wBtn, hBtn),
+              makeMaterialButton(true, wBtn, hBtn),
+            ]
+          )
+        ],
+      ),
     );
   }
 }
 
-class Home extends StatefulWidget {
-  Home({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  int _qId = 0;
-
-  void _incrementQuestions() {
-    setState(() {
-      _qId++;
-    });
-  }
+class App extends StatelessWidget {
+  final Map<int,String> _tree = const<int,String> {
+    1: 'Is it an animal?',
+    2: 'apple',
+    3: 'cat',
+  };
 
   @override
-  Widget build(BuildContext ctx) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: rsTitle,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'I have asked this many questions:',
-            ),
-            Text(
-              '$_qId',
-              style: Theme.of(ctx).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementQuestions,
-        tooltip: 'Answer question',
-        child: Icon(Icons.add),
-      ),
+      home: YesNoLayout(tree: _tree, ki: 1),
     );
   }
 }
